aaa9f7189b3300e1f1c4e31d4b80d0c60670fc5c
mmert9008 <michael.mertens.jr@gmail.com>
2025-05-31 21:04:13 -0500

    finished deep from problem set 1

--- Problem Set 1/deep/deep.py
+++ Problem Set 1/deep/deep.py
@@ -5,7 +5,15 @@ https://cs50.harvard.edu/python/psets/1/deep/
 """
 
 def main():
-    pass
+    user_input = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+    if user_input == "42":
+        print("Yes")
+    elif user_input == "forty-two":
+        print("Yes")
+    elif user_input == "forty two":
+        print("Yes")
+    else:
+        print("No")
 
 if __name__ == "__main__":
     main()
