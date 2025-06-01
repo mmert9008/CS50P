98d78605a755a3f2acf5cb57704bce15d44e0f07
mmert9008 <michael.mertens.jr@gmail.com>
2025-05-31 20:36:55 -0500

    added problem set 1 files, not actually done yet

---
+++ Problem Set 1/deep/deep.py
@@ -0,0 +1,9 @@
+"""
+In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
